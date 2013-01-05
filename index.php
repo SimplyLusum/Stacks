7f8ef00871bfc90220f6bfb3889b54c84c6fc641
Redfrost <contact@simonalex.com>
2013-01-06 12:42:28 +1300

    V1.0

---
+++ index.php
@@ -0,0 +1,190 @@
+<!DOCTYPE HTML>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        
+    	<!-- Site info -->      
+        <title>Lusum List</title>
+        <meta name="description" content="">
+        <meta name="author" content="">
+		<meta name="publisher" content=""/>
+		<meta name="keywords" content="">       
+
+		<!-- Icons -->
+		<link rel="shortcut icon" href="favicon.ico">
+		<link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+		<!-- Mobile View -->     
+    	<meta name="viewport" content="width=device-width">
+
+		<!-- Old Brow-fluidser Support -->
+		<script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+		
+		<!-- Style -->
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/bootstrap-responsive.css">
+        <link rel="stylesheet" href="css/font-awesome.css">
+        <link rel="stylesheet" href="css/main.css">
+        
+    </head>
+
+    
+    <body>
+
+	<div id="master-wrap">
+	
+		<!-- Header -->
+        <header class="navbar navbar-fixed-top">
+            <div class="navbar-inner">
+                <div class="container">
+                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </a>
+                    
+                    <!-- Logo -->
+                    <a class="brand" href="#"><strong>LUSUM LIST</strong></a>
+                    
+                    <!-- Navigation -->
+                    <nav class="nav-collapse collapse">
+                        <ul class="nav">
+                            <li><a href="#about">About</a></li>
+                            <li><a href="#services">Services</a></li>
+                            <li><a href="#contact">Contact</a></li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Action</a></li>
+                                    <li><a href="#">Another action</a></li>
+                                    <li><a href="#">Something else here</a></li>
+                                    <li class="divider"></li>
+                                    <li class="nav-header">Nav header</li>
+                                    <li><a href="#">Separated link</a></li>
+                                    <li><a href="#">One more separated link</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                        
+                        <!-- Form -->
+                        <form class="navbar-form pull-right">
+                            <input class="span2" type="text" placeholder="Email">
+                           
+                            <button type="submit" class="btn">Get Newsletters</button>
+                        </form>
+                    </nav><!--/.nav-collapse -->
+                </div>
+            </div>
+        </header><!-- /.navbar -->
+
+		<!-- Content -->
+        <div id="main-content" class="container">
+
+            <!-- Main hero unit for a primary marketing message or call to action -->
+            <section class="hero-unit">
+                <h1>Hello, world!</h1>
+                <p>This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+                <p><a class="btn btn-primary btn-large">Learn more &raquo;</a></p>
+            </section>
+
+
+			<section id="about">
+			<div class="row-fluid">
+				<div class="span8"><h2>About Us</h2><p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p><p>Yut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p><a href="#">Contact Info &raquo;</a></div>
+				<div class="span4"><img src="img/image_01.jpg" alt="" /></div>							
+			</div>
+			</section>
+			
+			
+			<hr>
+
+
+            <section id="services">
+			<div class="row-fluid">            
+                <div class="span4">
+                    <h2>Service 1</h2>
+                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                    <p><a class="btn" href="#">View details &raquo;</a></p>
+                </div>
+                <div class="span4">
+                    <h2>Service 2</h2>
+                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                    <p><a class="btn" href="#">View details &raquo;</a></p>
+               </div>
+                <div class="span4">
+                    <h2>Service 3</h2>
+                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                    <p><a class="btn" href="#">View details &raquo;</a></p>
+                </div>
+            </div>    
+            </section>
+         
+            
+           <hr> 
+         
+            
+           <section id="contact">
+           <div class="row-fluid">           
+				<div class="span5 map">
+				
+				<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.co.nz/maps?oe=utf-8&amp;client=firefox-a&amp;q=28+customs+street+east&amp;ie=UTF8&amp;hq=&amp;hnear=28+Customs+St+E,+Auckland,+1010&amp;gl=nz&amp;t=m&amp;source=embed&amp;ll=-36.836905,174.769392&amp;spn=0.024043,0.036478&amp;z=14&amp;iwloc=A&amp;output=embed">
+				</iframe>
+				
+				
+				</div>	
+				<div class="span7"><h2>Contact Us</h2><p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p>
+				
+				<p><strong>ADDRESS:</strong><br />
+				36 Praesent commodo cursus magna<br />
+				Laputa, Glubbdubdrib<br />
+				Houyhnhnms.</p>
+				<p>PH: 123-456-7890<br />
+				FAX: 123-456-7890
+				</p>
+				
+				<p><a class="btn btn-primary">Send Emails &raquo;</a></p>
+				
+				
+				</div>							
+		  </div>
+		  </section>
+
+        </div> <!-- /#main-content-->   
+        
+        
+        <!-- Footer -->
+        <footer id="footer">          
+          <section class="container">
+          <p>&copy; <?php echo date("Y") ?> Lusum List</p>
+          <p id="maker">Lusum List</p>
+          <p><a href="#">Back To Top</a></p>
+          </section>
+        </footer><!-- /#footer -->
+        
+        
+	</div><!-- /#master-wrap-->
+	
+	
+	
+		<!-- Scripts -->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.3.min.js"><\/script>')</script>
+        <script src="js/vendor/bootstrap.js"></script>
+        <script src="js/main.js"></script>
+        <script src="js/smooth-scroll.min.js"></script><!-- Smooth Scroll -->
+        
+
+		<!-- Analytics -->
+        <script>
+            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
+        
+    </body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Smooth Scroll
+		    $(document).ready(function() {
+		    
+		      $('.nav a').smoothScroll({offset: -100});
+		    
+		    });
