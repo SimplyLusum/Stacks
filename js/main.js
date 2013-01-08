fdb84484287e5d920fd039c594aa28b8b997d732
Redfrost <contact@simonalex.com>
2013-01-08 17:35:53 +1300

    License
    - License added
    - Wufoo Form support

--- js/main.js
+++ js/main.js
@@ -7,3 +7,29 @@
 
 // Scrollspy offset
 $('[data-spy="scroll"]').scrollspy({ offset: 100 });
+
+
+// Tooltip
+	  jQuery(document).ready(function () {
+	    $("[rel=tooltip]").tooltip();
+	  });
+
+	
+// Popover
+	  jQuery(document).ready(function () {
+	    $("[rel=popover]").popover();
+	  });
+
+
+// Carousel	Auto
+		jQuery(document).ready(function() {
+			 $('#myCarousel-auto').carousel({
+				   interval: 5000,
+					  cycle: true
+			 });
+		});
+		var $ = jQuery.noConflict();
+		
+		
+		
+		
