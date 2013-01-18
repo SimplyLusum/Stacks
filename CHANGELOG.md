ae7c7b28e4020cd860caf874ee08bea0c711fb83
Redfrost <contact@simonalex.com>
2013-01-18 18:26:07 +1300

    V1.2
    - Div background cufoff issue: solved (body min-width)
    - Responsive Layout support
    - Responsive Nav menu
    - Responsive Hero-unit
    - Responsive Footer
    - Responsive Popover
    - Removed unnecessary code in responsive.css

---
+++ CHANGELOG.md
@@ -0,0 +1,35 @@
+### 1.2: 18-Jan-2013
+* Div background cuf-off issue: solved (body min-width)
+* Responsive Layout support
+* Responsive Nav menu
+* Responsive Hero-unit
+* Responsive Footer
+* Responsive Popover
+* Responsive Div margin
+* Removed unnecessary code in responsive.css 
+* Changelog
+
+
+### 1.11: 07-Jan-2013
+* Style Enhancement
+* Korean page + Style testing
+* Footer menu change
+* Icon change
+* License doc
+* Custom Wufoo Form support
+
+
+### 1.1: 06-Jan-2013
+* hero-unit jumbotron
+* font-awsome-more (http://gregoryloucas.github.com/Font-Awesome-More/)
+* SNS icons (http://www.zurb.com/playground/social-webicons)
+* nav menu scrollspy
+* footer menu
+* footer menu mobile version 
+
+
+### 1.0: 05-Jan-2013
+* Bootstrap 2.2.2 (custom modified + bug patch)
+
+
+
--- responsive.php
+++ responsive.php
@@ -40,7 +40,7 @@ When you look into an abyss, the abyss also looks into you.
     </head>
 
     
-	<body id="home" data-spy="scroll">
+	<body id="home" class="responsive" data-spy="scroll">
 
 	<div id="master-wrap">
 	
@@ -95,12 +95,16 @@ When you look into an abyss, the abyss also looks into you.
 
 		
 			<!-- Main hero unit for a primary marketing message or call to action -->
-            <section class="hero-unit jumbotron masthead">	            
-	            <div class="container">
-            	<h1>Hello, world!</h1>
-            	<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
-            	<p><a class="btn btn-info btn-large">Learn more &raquo;</a></p>
-            	</div>            	
+            <section class="hero-unit jumbotron masthead">
+            	<div class="container">           
+			      <div class="row-fluid"> 
+			        <div class="span12">  
+	            	<h1>Hello, world!</h1>
+	            	<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Aenean eu leo quam.</p>
+	            	<p><a class="btn btn-info btn-large">Learn more &raquo;</a></p>
+	            	</div>    
+            	  </div>     
+            	</div>    	
             </section>
 
 
@@ -108,7 +112,19 @@ When you look into an abyss, the abyss also looks into you.
         <div id="main-content" class="container">
 			<section id="about">
 			<div class="row-fluid">
-				<div class="span7"><h2>About Us</h2><p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p><p>Yut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p><a href="#">Contact Info &raquo;</a></div>
+				<div class="span7"><h2>Simple &amp; Intuitive Design</h2>
+					<p class="byline">Pellentesque ornare sem</p>
+					
+					<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>					
+					
+					<ul class="tick_box">
+					<li><i class="icon-ok"></i> <strong>Pellentesque ornare</strong></li>
+					<li><i class="icon-ok"></i> <strong>Sem lacinia quam venenatis</strong></li>
+					<li><i class="icon-ok"></i> <strong>Vestibulum</strong></li>
+					</ul>
+					
+					
+					<a href="#">View Gallery &raquo;</a></div>
 				<div class="span5"><img src="img/image_01.jpg" alt="" /></div>							
 			</div>
 			</section>
@@ -120,21 +136,21 @@ When you look into an abyss, the abyss also looks into you.
             <section id="services">
 			<div class="row-fluid">            
                 <div class="span4">
-                    <img src="img/image_01.jpg" alt="" />
-                    <h2>Service 1</h2>
-                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                    <img src="img/image_02.jpg" alt="" />
+                    <h2>Inspirational Word</h2>
+                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum.</p>
                     <p><a class="btn" href="#">View details &raquo;</a></p>
                 </div>
                 <div class="span4">
-	                <img src="img/image_01.jpg" alt="" />
-                    <h2>Service 2</h2>
-                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+	                <img src="img/image_03.jpg" alt="" />
+                    <h2>Creative Thinking</h2>
+                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum.</p>
                     <p><a class="btn" href="#">View details &raquo;</a></p>
                </div>
                 <div class="span4">
-	                <img src="img/image_01.jpg" alt="" />
-                    <h2>Service 3</h2>
-                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+	                <img src="img/image_04.jpg" alt="" />
+                    <h2>Passionate Action</h2>
+                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum.</p>
                     <p><a class="btn" href="#">View details &raquo;</a></p>
                 </div>
             </div>    
@@ -145,18 +161,28 @@ When you look into an abyss, the abyss also looks into you.
          
             
            <section id="contact">
-           <div class="row-fluid">           
-				<div class="span7">
-					<h2>Contact Us</h2><p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p>
+           					<h2><i class="icon-comments-alt"></i> Got Questions?</h2>
+					<p class="byline">Help and support</p>
 					
-					<p><strong>ADDRESS:</strong><br />
-					36 Praesent commodo cursus magna<br />
+					<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Etiam porta sem malesuada magna.</p>
+           <div class="row-fluid">       
+               
+				<div class="span7">
+
+					
+					<div class="well">
+					<h3><strong>LOCATION:</strong></h3>
+					<p>36 Praesent commodo cursus magna<br />
 					Laputa, Glubbdubdrib<br />
 					Houyhnhnms.</p>
 					<p>PH: 123-456-7890<br />
 					FAX: 123-456-7890
-					</p>					
-					<p><a class="btn btn-info btn-large">Send Emails &raquo;</a></p>				
+					</p>		
+					<h6><em>*Scelerisque nisl consectetur et.</em></h6>		
+						
+					</div>
+							
+					<a id="popup_trigger" class="btn btn-info btn-large" href="#popup" data-toggle="modal">Enquiry &raquo;</a><!-- Popup trigger -->   				
 				</div>						
 				
 				<div class="span5 map">			
@@ -177,10 +203,20 @@ When you look into an abyss, the abyss also looks into you.
 	  		<!-- Footer menu -->
 	  		<div id="footer_menu">
       			<ul id="footer_menu_box">
-      			  	<li><a href="#">Company Info</a></li>
-      			  	<li><a href="#">Privacy</a></li>
-      			  	<li><a href="#">Terms</a></li>
-      			  	<li><a href="#">Contact Info</a></li>
+      			  	<li><a href="https://github.com/redfrost/Stacks">Download</a></li>
+                    <li><a onmouseover="this.style.cursor='pointer'" rel="popover" data-placement="top" data-content="Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE." title="Copyright &copy; 2013 Redfrost">License</a></li>
+
+      			  	
+      			  	<li><a href="index.php">English</a></li>
+      			  	<li><a href="index2.php">한국어</a></li>
+      			  	<li><a href="index.php">Fixed Layout</a></li>
+      			  	<!-- <li><a onmouseover="this.style.cursor='pointer'" rel="tooltip" data-placement="top" title="In Progress">Go Responsive</a></li> -->
+      			  	                    
+      			  	                    
       			</ul>
       		</div>
 
@@ -209,13 +245,60 @@ When you look into an abyss, the abyss also looks into you.
 	
 	
 	
+	
+	
+			<!-- Modal Popup: Contact Form | You must replace this section with your own form. -->
+			<div id="popup" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+		  
+				  <div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+					<h3 id="myModalLabel">Contact Us</h3>
+				  </div>
+										  							  
+				<div id="contact_form" class="ltr">
+				
+				<form id="form1" name="form1" class="wufoo topLabel page" autocomplete="off" enctype="multipart/form-data" method="post" novalidate
+				action="https://simonalex.wufoo.com/forms/z7x3x5/#public">
+				
+					<ul>			
+						<li id="foli5" class="notranslate">
+						<label class="desc" id="title5" for="Field5">Name<span id="req_5" class="req">*</span></label>
+						<span><input id="Field5" name="Field5" type="text" class="field text fn" value="First Name" size="8" tabindex="1" required /></span>
+						<span><input id="Field6" name="Field6" type="text" class="field text ln" value="Last Name" size="14" tabindex="2" required /></span>
+						</li>
+						
+						<li id="foli7" class="notranslate"><label class="desc" id="title7" for="Field7">Email
+						<span id="req_7" class="req">*</span></label>
+						<div><input id="Field7" name="Field7" type="email" spellcheck="false" class="field text medium" value="" maxlength="255" tabindex="3" required /></div></li>
+						
+						<li id="foli4" class="notranslate"><label class="desc" id="title4" for="Field4">Subject</label>
+						<div><input id="Field4" name="Field4" type="text" class="field text medium" value="" maxlength="255" tabindex="4" onkeyup="" /></div></li>
+						<li id="foli8" class="notranslate"><label class="desc" id="title8" for="Field8">Message</label>
+						
+						<div><textarea id="Field8" name="Field8" class="field textarea medium" spellcheck="true" rows="10" cols="50" tabindex="5" onkeyup="" ></textarea></div></li> 
+						<input id="saveForm" name="saveForm" class="btn btn-large btn-info btTxt submit" type="submit" value="Submit »" />
+						
+						<li class="hide">
+						<label for="comment">Do Not Fill This Out</label>
+						<textarea name="comment" id="comment" rows="1" cols="1"></textarea>
+						<input type="hidden" id="idstamp" name="idstamp" value="2K4l2NUNrKXsGwMdJkS37DEWU57UC2YJH05gY+V50og=" />
+						</li>			
+					</ul>
+				</form> 
+				
+				</div><!-- /#contact_form -->
+			
+			</div><!-- /#popup -->
+			
+	
+	
 		<!-- Scripts -->
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.3.min.js"><\/script>')</script>
         <script src="js/vendor/script.js"></script>
         <script src="js/main.js"></script>
         <script src="js/smooth-scroll.min.js"></script><!-- Smooth Scroll -->
-		<script src="js/wufoo.js"></script><!-- Form control -->        
+		<script src="js/wufoo.js"></script><!-- Form control -->  
         
 
 		<!-- Analytics -->
