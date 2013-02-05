0dc752b063953e28faca1347a3fe55a4c30310dd
Redfrost <contact@simonalex.com>
2013-02-05 17:56:52 +1300

    jQuery 1.9.1 Update

--- index2.php
+++ index2.php
@@ -293,8 +293,8 @@ THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR I
 	
 	
 		<!-- Scripts -->
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/jquery-1.8.3.min.js"><\/script>')</script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/jquery-1.9.1.min.js"><\/script>')</script>
         <script src="js/script.js"></script>
         <script src="js/custom.js"></script>
         <script src="js/smooth-scroll.min.js"></script><!-- Smooth Scroll -->
