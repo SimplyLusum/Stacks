b4aff27637ff7b6fe6d56404f50fef9d821a1449
Redfrost <contact@simonalex.com>
2013-02-05 17:51:03 +1300

    Section Template Foundation

---
+++ section/hero.php
@@ -0,0 +1,11 @@
+            <section class="hero-unit jumbotron masthead">
+            	<div class="container">           
+			      <div class="row-fluid"> 
+			        <div class="span12">  
+	            	<h1>Hello, world!</h1>
+	            	<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Aenean eu leo quam.</p>
+	            	<p><a class="btn btn-info btn-large">Learn more &raquo;</a></p>
+	            	</div>    
+            	  </div>     
+            	</div>    	
+            </section>
